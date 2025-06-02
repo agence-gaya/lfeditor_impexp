30c6849d27d68850ebf703e559837145d4250076
Benoit Chenu <bchenu@gaya.fr>
2025-06-02 12:26:05 +0200

    [TASK] Remove deprecated ext_ files

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'extension-lfeditor_impexp-ext-icon' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:lfeditor_impexp/Resources/Public/Icons/Extension.svg',
+    ],
+];
